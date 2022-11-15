085c74882a24bd8a3da225a090c4bc12f88d2901
Messih Grmay <mesihgrmay@yahoo.com>
2022-11-14 19:08:08 -0800

    Task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""For a given employee ID, returns his/her TODO list progress"""
+import json
+import requests
+import sys
+
+
+url = "https://jsonplaceholder.typicode.com"
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        id = int(sys.argv[1])
+        users = requests.get("{}/users".format(url)).json()
+        with open("todo_all_employees.json", "w") as jsonfile:
+            json.dump({
+                u.get("id"): [{
+                    "task": t.get("title"),
+                    "completed": t.get("completed"),
+                    "username": u.get("username")
+                } for t in requests.get(url + "/todos",
+                                        params={"userId": u.get("id")}).json()]
+                for u in users}, jsonfile)
