4b64400ed370d28123b310364ba33a4ebee1ff51
Messih Grmay <mesihgrmay@yahoo.com>
2022-11-29 09:48:55 -0800

    Task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,15 +8,14 @@ def recurse(subreddit, hot_list=[], after=None):
     url = 'http://www.reddit.com/r/{}/hot.json'.format(subreddit)
     header = {'User-Agent': '0x16-api_advanced/mg1020'}
     param = {'after': after}
-    response = requests.get(url, headers=header, params=param)
-    if response.status_code == 200:
-        data = response.json().get("data", {}).get("children", {})
-        after = response.json().get("data", {}).get("after", {})
-        if data is None or after is None:
-            return None
-        for item in data:
-            hot_list.append(item.get('data').get('title'))
-        return hot_list
-
-    else:
+    response = requests.get(url, headers=header, params=param,
+                            allow_redirects=False)
+    if response.status_code != 200:
+        return None
+    data = response.json().get("data")
+    after = data.get("after")
+    for item in data.get('children'):
+        hot_list.append(item.get('data').get('title'))
+    if after is not None:
         return recurse(subreddit, hot_list, after)
+    return hot_list
