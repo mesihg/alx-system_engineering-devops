3acd916c6a7c567cb9a48b4a233c036dfe2723c7
Messih Grmay <mesihgrmay@yahoo.com>
2022-11-29 08:40:51 -0800

    Task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Function that queries the Reddit API """
+import requests
+
+
+def top_ten(subreddit):
+    """prints the titles of the first 10 hot posts listed for subreddit"""
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    header = {'User-Agent': '0x16-api_advanced/mg1020'}
+    params = {'limit': 10}
+    response = requests.get(url, headers=header, params=params)
+    if response.status_code == 200:
+        titles = response.json().get("data", {}).get("children", {})
+        for title in titles:
+            print(title.get('data').get('title'))
+    else:
+        print(None)
